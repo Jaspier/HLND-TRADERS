4fb00290a4621f2591c8594a50b31c38a7a8842b
Jaspier <57241026+Jaspier@users.noreply.github.com>
2021-02-05 01:47:26 +0000

    Basic frontend implemented

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Carabaos of Wall Street</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/join'>
+                <Nav.Link>
+                  <i className='fas fa-user-friends'></i> Join us!
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/pricings'>
+                <Nav.Link>
+                  <i className='fas fa-hand-holding-usd'></i> Pricings
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Basic Plan',
+    image: '/images/basic.jpg',
+    description:
+      'Basic Plan - for traders looking to potentially earn some pocket cash.',
+    price: 0.0,
+  },
+  {
+    _id: '2',
+    name: 'Premium Plan',
+    image: '/images/premium.jpg',
+    description: 'Premium Plan - for traders looking to make some REAL money!',
+    price: 15.0,
+  },
+];
+
+export default products;
