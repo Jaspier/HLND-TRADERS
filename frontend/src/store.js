014697f412a27337a23d9ad5b26f8654667c83fe
Jaspier <57241026+Jaspier@users.noreply.github.com>
2021-02-06 17:17:06 +0000

    Implemented Front End User Authentication & User Profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,34 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { joinReducer } from './reducers/joinReducers';
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   join: joinReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 const joinItemFromStorage = localStorage.getItem('joinItems')
   ? JSON.parse(localStorage.getItem('joinItems'))
   : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   join: { joinItems: joinItemFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
